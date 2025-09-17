eb4707f861db963c1c1a68555ee60bf24758ea77
syderkkk <italomendoza2003@gmail.com>
2025-09-17 15:07:34 -0500

    modify views

--- src/inmuebles/views.py
+++ src/inmuebles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Propiedad
+from .forms import PropiedadForm
 
 # Create your views here.
 
@@ -9,3 +10,14 @@ def listar_propiedades(request):
     return render(request, 'inmuebles/listar_inmuebles.html', {
         'propiedades': propiedades,
     })
+
+
+def crear_propiedad(request):
+    if request.method == 'POST':
+        form = PropiedadForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_propiedades')
+    else:
+        form = PropiedadForm()
+    return render(request, 'inmuebles/crear_inmuebles.html', {'form': form})
