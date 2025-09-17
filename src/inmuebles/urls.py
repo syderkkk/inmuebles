b70387a01209e272a083d9c48f9a7fafe5520d18
syderkkk <italomendoza2003@gmail.com>
2025-09-17 15:07:28 -0500

    modify urls

--- src/inmuebles/urls.py
+++ src/inmuebles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.listar_propiedades, name="propiedades")
+    path("", views.listar_propiedades, name="listar_propiedades"),
+    path("crear/", views.crear_propiedad, name="crear_propiedad")
 ]
